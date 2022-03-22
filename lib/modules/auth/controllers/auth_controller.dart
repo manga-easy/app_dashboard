af2a6edb59e8680338a1a5919419c7dfb358476e
lucas <lucas.cardo123@hotmail.com>
2022-03-21 20:07:29 -0400

    add campos de autenticação

--- lib/modules/auth/controllers/auth_controller.dart
+++ lib/modules/auth/controllers/auth_controller.dart
@@ -1,9 +1,12 @@
+import 'package:dashboard_manga_easy/core/services/appwrite_admin.dart';
+import 'package:dashboard_manga_easy/core/services/appwrite_client.dart';
 import 'package:dashboard_manga_easy/core/services/global.dart';
 import 'package:dashboard_manga_easy/modules/main/views/main_screen.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
   final gb = Get.find<Global>();
+  final app = Get.find<AppwriteClient>();
   @override
   void onClose() {
     super.onClose();
@@ -15,6 +18,11 @@ class AuthController extends GetxController {
   }
 
   logar() {
-    Get.toNamed(MainScreen.router);
+    try {
+    app.account.createSession(email: email, password: password)
+    Get.offAllNamed(MainScreen.router);
+     } catch (e) {
+       Get.defaultDialog(title: 'Erro', middleText: e.toString());
+    }
   }
 }
--- lib/modules/auth/views/auth_page.dart
+++ lib/modules/auth/views/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:dashboard_manga_easy/modules/auth/controllers/auth_controller.dart';
 import 'package:dashboard_manga_easy/modules/main/views/widgets/button_padrao.dart';
+import 'package:dashboard_manga_easy/modules/main/views/widgets/campo_padrao.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,11 +11,19 @@ class AuthPage extends GetView {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ButtonPadrao(
-        title: 'logar',
-        icone: Icons.dangerous,
-        onPress: ct.logar,
-      ),
-    );
+        body: ListView(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      children: [
+        CampoPadrao(hintText: 'E-mail'),
+        SizedBox(height: 10),
+        CampoPadrao(hintText: 'Senha'),
+        SizedBox(height: 10),
+        ButtonPadrao(
+          title: 'logar',
+          icone: Icons.dangerous,
+          onPress: ct.logar,
+        ),
+      ],
+    ));
   }
 }
