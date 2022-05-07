60141f5b3a15313fdea5e4047cd9a3b3a080b833
lucas <lucas.cardo123@hotmail.com>
2022-05-07 01:23:41 -0400

    add template para pagina de modulo

--- lib/modules/dashboard/templates/modulo_page_template.dart
+++ lib/modules/dashboard/templates/modulo_page_template.dart
@@ -3,13 +3,14 @@ import 'package:dashboard_manga_easy/core/config/responsive.dart';
 import 'package:dashboard_manga_easy/core/services/global.dart';
 import 'package:dashboard_manga_easy/modules/dashboard/atoms/campo_padrao_atom.dart';
 import 'package:dashboard_manga_easy/modules/dashboard/atoms/loading_atom.dart';
+
 import 'package:dashboard_manga_easy/modules/dashboard/widgets/side_menu.dart';
-import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
 
 class ModuloPageTemplate extends StatelessWidget {
   final String route;
-  final ValueListenable<StatusBuild> valueListenable;
+  final StatusBuild statusBuild;
   final void Function()? onPressedAtualiza;
   final void Function()? onPressedNovoItem;
   final String labelNovoItem;
@@ -21,7 +22,7 @@ class ModuloPageTemplate extends StatelessWidget {
   const ModuloPageTemplate({
     Key? key,
     required this.route,
-    required this.valueListenable,
+    required this.statusBuild,
     this.onPressedAtualiza,
     this.onPressedNovoItem,
     required this.labelNovoItem,
@@ -35,48 +36,47 @@ class ModuloPageTemplate extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: SideMenu(atual: route),
-      appBar: AppBar(),
+      appBar: AppBar(title: Text(route.replaceAll('/', ''))),
       body: SafeArea(
-        child: ValueListenableBuilder<StatusBuild>(
-          valueListenable: valueListenable,
-          builder: (context, value, child) {
-            if (value == StatusBuild.loading) {
-              return const LoadingAtom();
-            }
-            return Column(
-              children: [
-                const SizedBox(height: AppTheme.defaultPadding),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: statusBuild == StatusBuild.loading
+            ? const LoadingAtom()
+            : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Column(
                   children: [
-                    TextButton(onPressed: onPressedAtualiza, child: const Text('Atualiza')),
-                    ElevatedButton.icon(
-                      style: TextButton.styleFrom(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: AppTheme.defaultPadding * 1.5,
-                          vertical: AppTheme.defaultPadding / (Responsive.isMobile(context) ? 2 : 1),
+                    const SizedBox(height: AppTheme.defaultPadding),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(onPressed: onPressedAtualiza, child: const Text('Atualiza')),
+                        ElevatedButton.icon(
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: AppTheme.defaultPadding * 1.5,
+                              vertical: AppTheme.defaultPadding / (Responsive.isMobile(context) ? 2 : 1),
+                            ),
+                          ),
+                          onPressed: onPressedNovoItem,
+                          icon: const Icon(Icons.add),
+                          label: Text(labelNovoItem),
                         ),
+                      ],
+                    ),
+                    const SizedBox(height: AppTheme.defaultPadding),
+                    CampoPadraoAtom(
+                      onChange: onChangePesquisa,
+                      onEditComplet: onEditCompletPesquisa,
+                    ),
+                    const SizedBox(height: AppTheme.defaultPadding / 2),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: listaItems.length,
+                        itemBuilder: itemBuilderLista,
                       ),
-                      onPressed: onPressedNovoItem,
-                      icon: const Icon(Icons.add),
-                      label: Text(labelNovoItem),
                     ),
                   ],
                 ),
-                CampoPadraoAtom(
-                  onChange: onChangePesquisa,
-                  onEditComplet: onEditCompletPesquisa,
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: listaItems.length,
-                    itemBuilder: itemBuilderLista,
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
+              ),
       ),
     );
   }
