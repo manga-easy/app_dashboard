ffc6b46ffa1e5f9c81805dda8b35097bd109a8b7
lucas cardoso <lucas.cardo123@hotmail.com>
2022-06-29 11:18:04 -0400

    Add page forbiden

---
+++ lib/modules/dashboard/views/forbiden_page.dart
@@ -0,0 +1,36 @@
+import 'package:dashboard_manga_easy/core/services/global.dart';
+import 'package:dashboard_manga_easy/modules/dashboard/templates/modulo_page_template.dart';
+import 'package:flutter/material.dart';
+
+class ForbidenPage extends StatefulWidget {
+  static const route = '/Forbiden';
+  const ForbidenPage({Key? key}) : super(key: key);
+  @override
+  State<ForbidenPage> createState() => _ForbidenPageState();
+}
+
+class _ForbidenPageState extends State<ForbidenPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ModuloPageTemplate(
+      route: ForbidenPage.route,
+      statusBuild: StatusBuild.done,
+      itemBuilderLista: (context, index) {
+        return const Center(
+          child: Text('Sem permissão para acessar esse modulo'),
+        );
+      },
+      listaItems: const [1],
+    );
+  }
+}
