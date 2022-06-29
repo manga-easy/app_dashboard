47bc8400ac03472ed5dce890b087e50a0a297c78
lucas cardoso <lucas.cardo123@hotmail.com>
2022-06-29 10:06:18 -0400

    register repo credencial and user repos

--- lib/modules/auth/auth_module.dart
+++ lib/modules/auth/auth_module.dart
@@ -1,15 +1,20 @@
 import 'package:dashboard_manga_easy/core/interfaces/module_factory.dart';
 import 'package:dashboard_manga_easy/main.dart';
 import 'package:dashboard_manga_easy/modules/auth/controllers/auth_controller.dart';
+import 'package:dashboard_manga_easy/modules/auth/domain/repo/credencial_repo.dart';
+import 'package:dashboard_manga_easy/modules/auth/domain/repo/user_repo.dart';
 
 class AuthModule extends IModuleFactory {
   @override
   void register() {
+    //register repositories
+    di.registerFactory(() => CredencialRepo(di()));
+    di.registerFactory(() => UserRepo(di()));
     //register controllers
     di.registerFactory(
       () => AuthController(
         app: di(),
-        gb: di(),
+        serviceRoute: di(),
         credencialRepo: di(),
       ),
     );
