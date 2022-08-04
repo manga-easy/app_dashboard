77f053be3b3f427239b8e60e5f0413fd3c1f6b90
lucas cardoso <lucas.cardo123@hotmail.com>
2022-08-04 13:26:58 -0400

    Add api EasyComic

---
+++ lib/modules/autor/domain/api_easy_comic.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class ApiEasyComic {
+  final String urlBase = 'http://localhost:8083';
+  final client = Client();
+
+  Future<Map<String, dynamic>?> get(String path) async {
+    var ret = await client.get(Uri.parse(urlBase + path));
+    return json.decode(ret.body);
+  }
+
+  Future<Map<String, dynamic>?> post(String path, Map<String, dynamic> body) async {
+    var ret = await client.post(Uri.parse(urlBase + path), body: body);
+    return json.decode(ret.body);
+  }
+
+  Future<Map<String, dynamic>?> put(String path, Map<String, dynamic> body) async {
+    var ret = await client.put(Uri.parse(urlBase + path), body: body);
+    return json.decode(ret.body);
+  }
+
+  Future<Map<String, dynamic>?> delete(String path, Map<String, dynamic> body) async {
+    var ret = await client.delete(Uri.parse(urlBase + path), body: body);
+    return json.decode(ret.body);
+  }
+}
