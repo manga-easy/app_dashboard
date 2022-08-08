803170e25ecad1432baf61d4510475b726731c5b
lucas cardoso <lucas.cardo123@hotmail.com>
2022-08-08 14:52:08 -0400

    Add attributes for form in edit_comic

--- lib/modules/autor/presenter/controllers/edit_comic_controller.dart
+++ lib/modules/autor/presenter/controllers/edit_comic_controller.dart
@@ -1,16 +1,30 @@
 import 'package:dashboard_manga_easy/core/interfaces/controller.dart';
+import 'package:dashboard_manga_easy/modules/autor/domain/comic_model.dart';
 import 'package:flutter/material.dart';
 
-class EditComicController extends IController {
-  EditComicController();
+class EditComicController extends ValueNotifier implements IController {
+  ComicModel? comic;
+
+  EditComicController() : super(null);
 
   @override
   void onClose() {
-    // TODO: implement onClose
+    dispose();
   }
 
   @override
   void onInit(BuildContext context) {
-    // TODO: implement onInit
+    comic = ModalRoute.of(context)!.settings.arguments as ComicModel?;
+    comic ??= ComicModel(
+      autor: '',
+      chapter: [],
+      cover: '',
+      scans: '',
+      sinopse: '',
+      status: true,
+      title: '',
+      uniqueid: '',
+      yearUp: 0,
+    );
   }
 }
--- lib/modules/autor/presenter/ui/pages/edit_comic_page.dart
+++ lib/modules/autor/presenter/ui/pages/edit_comic_page.dart
@@ -1,5 +1,9 @@
+import 'package:dashboard_manga_easy/core/config/app_theme.dart';
 import 'package:dashboard_manga_easy/main.dart';
 import 'package:dashboard_manga_easy/modules/autor/presenter/controllers/edit_comic_controller.dart';
+import 'package:dashboard_manga_easy/modules/dashboard/atoms/button_padrao_atom.dart';
+import 'package:dashboard_manga_easy/modules/dashboard/atoms/campo_padrao_atom.dart';
+import 'package:dashboard_manga_easy/modules/dashboard/atoms/loading_atom.dart';
 import 'package:flutter/material.dart';
 
 class EditComicPage extends StatefulWidget {
@@ -26,6 +30,56 @@ class _EditComicPageState extends State<EditComicPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Criar Comic'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(AppTheme.defaultPadding),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ValueListenableBuilder(
+          valueListenable: ct,
+          builder: (context, value, child) {
+            if (ct.comic == null) return const LoadingAtom();
+            return ListView(
+              padding: const EdgeInsets.symmetric(horizontal: AppTheme.defaultPadding),
+              children: [
+                CampoPadraoAtom(
+                  hintText: 'Titulo',
+                  initialValue: ct.comic!.title,
+                  onChange: (p0) => ct.comic!.title = p0,
+                ),
+                const SizedBox(height: AppTheme.defaultPadding),
+                CampoPadraoAtom(
+                  hintText: 'Sinopse',
+                  initialValue: ct.comic!.sinopse,
+                  onChange: (p0) => ct.comic!.sinopse = p0,
+                ),
+                const SizedBox(height: AppTheme.defaultPadding),
+                CampoPadraoAtom(
+                  hintText: 'Link da capa',
+                  initialValue: ct.comic!.cover,
+                  onChange: (p0) => ct.comic!.cover = p0,
+                ),
+                const SizedBox(height: AppTheme.defaultPadding),
+                CampoPadraoAtom(
+                  hintText: 'Ano',
+                  initialValue: ct.comic!.yearUp.toString(),
+                  onChange: (p0) => ct.comic!.yearUp = int.tryParse(p0) ?? 0,
+                ),
+                const SizedBox(height: AppTheme.defaultPadding),
+                ButtonPadraoAtom(
+                  title: 'Salvar',
+                  icone: Icons.create,
+                  onPress: () => null,
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
