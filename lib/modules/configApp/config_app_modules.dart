84aa334461be659b5782e4e8b0f60226b20334b7
lucas <lucas.cardo123@hotmail.com>
2022-12-04 11:09:55 -0400

    Register cases and controller in config module

--- lib/modules/configApp/config_app_modules.dart
+++ lib/modules/configApp/config_app_modules.dart
@@ -2,10 +2,12 @@ import 'package:dashboard_manga_easy/core/interfaces/module_factory.dart';
 import 'package:dashboard_manga_easy/main.dart';
 import 'package:dashboard_manga_easy/modules/configApp/domain/repositories/config_app_repository_inter.dart';
 import 'package:dashboard_manga_easy/modules/configApp/domain/usercases/get_block_list_case.dart';
+import 'package:dashboard_manga_easy/modules/configApp/domain/usercases/get_config_app_case.dart';
 import 'package:dashboard_manga_easy/modules/configApp/domain/usercases/up_block_list_case.dart';
 import 'package:dashboard_manga_easy/modules/configApp/external/data_source_config.dart';
 import 'package:dashboard_manga_easy/modules/configApp/infra/datasources/data_source_config.dart';
 import 'package:dashboard_manga_easy/modules/configApp/infra/repositories/config_app_repository.dart';
+import 'package:dashboard_manga_easy/modules/configApp/presenter/controllers/config_app_controllers.dart';
 
 class ConfigAppModule extends IModuleFactory {
   @override
@@ -16,6 +18,9 @@ class ConfigAppModule extends IModuleFactory {
     di.registerFactory<IConfigAppRepository>(() => ConfigAppRepository(di()));
     //usecases
     di.registerFactory(() => GetBlockListCase(di()));
+    di.registerFactory(() => GetConfigAppCase(di()));
     di.registerFactory(() => UpBlockListCase(di()));
+    //controllers
+    di.registerFactory(() => ConfigAppController(getConfigAppCase: di()));
   }
 }
