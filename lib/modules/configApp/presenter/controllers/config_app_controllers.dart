8ce68ab8fb51f8ff7972e97cb8f0c5a871ffce1d
lucas <lucas.cardo123@hotmail.com>
2022-12-04 12:08:37 -0400

    Create view and controller config module

---
+++ lib/modules/configApp/presenter/controllers/config_app_controllers.dart
@@ -0,0 +1,53 @@
+import 'package:dashboard_manga_easy/core/config/app_helpes.dart';
+import 'package:dashboard_manga_easy/core/interfaces/controller.dart';
+import 'package:dashboard_manga_easy/core/services/global.dart';
+import 'package:dashboard_manga_easy/modules/configApp/domain/usercases/get_config_app_case.dart';
+import 'package:dashboard_manga_easy/modules/configApp/domain/usercases/up_config_app_case.dart';
+import 'package:flutter/material.dart';
+import 'package:sdk_manga_easy/sdk_manga_easy.dart';
+
+class ConfigAppController extends IController {
+  final GetConfigAppCase getConfigAppCase;
+  final UpConfigAppCase upConfigAppCase;
+
+  ConfigAppController({
+    required this.getConfigAppCase,
+    required this.upConfigAppCase,
+  });
+
+  var status = StatusBuild.loading;
+  var config = ConfigAppModel.offline();
+
+  @override
+  void init(BuildContext context) {
+    loadingConfig();
+  }
+
+  Future<void> loadingConfig() async {
+    try {
+      config = await getConfigAppCase();
+      status = StatusBuild.done;
+    } catch (e) {
+      Helps.log(e);
+      status = StatusBuild.erro;
+    }
+    notifyListeners();
+  }
+
+  Future<void> updateConfig(BuildContext context) async {
+    try {
+      await upConfigAppCase(config);
+      AppHelps.confirmaDialog(
+        title: 'Sucesso',
+        content: 'Config atualizado com sucesso',
+        context: context,
+      );
+    } catch (e) {
+      AppHelps.confirmaDialog(
+        title: 'Erro',
+        content: e.toString(),
+        context: context,
+      );
+    }
+  }
+}
