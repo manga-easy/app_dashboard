f5f43986deadb48c9b80b1f09f8204fb2764668c
lucas <lucas.cardo123@hotmail.com>
2022-12-11 19:32:41 -0400

    Create create_temporada_case.dart

---
+++ lib/modules/temporadas/domain/usercases/create_temporada_case.dart
@@ -0,0 +1,37 @@
+import 'package:dashboard_manga_easy/modules/configApp/domain/usercases/get_config_app_case.dart';
+import 'package:dashboard_manga_easy/modules/configApp/domain/usercases/up_config_app_case.dart';
+import 'package:dashboard_manga_easy/modules/temporadas/domain/repositories/temporadas_repository.dart';
+import 'package:manga_easy_sdk/manga_easy_sdk.dart';
+
+abstract class CreateTemporadaCase {
+  final GetConfigAppCase getConfigAppCase;
+  final UpConfigAppCase upConfigAppCase;
+  final TemporadasRepository _temporadasRepository;
+
+  CreateTemporadaCase(
+    this._temporadasRepository,
+    this.getConfigAppCase,
+    this.upConfigAppCase,
+  );
+
+  Future<void> call({required TemporadaModel temporada});
+}
+
+class CreateTemporadaCaseImpl extends CreateTemporadaCase {
+  CreateTemporadaCaseImpl(
+    super.temporadasRepository,
+    super.getConfigAppCase,
+    super.upConfigAppCase,
+  );
+
+  @override
+  Future<void> call({required TemporadaModel temporada}) async {
+    await _temporadasRepository.creatDocument(objeto: temporada);
+    updateConfigApp();
+  }
+
+  Future<void> updateConfigApp() async {
+    var ret = await getConfigAppCase();
+    await upConfigAppCase(ret);
+  }
+}
