74a760f0146dc7300ed6d378cdfe1526a9b98d73
lucas <lucas.cardo123@hotmail.com>
2023-07-08 22:02:25 -0400

    add handler Error in controller

--- lib/core/interfaces/controller.dart
+++ lib/core/interfaces/controller.dart
@@ -1,3 +1,4 @@
+import 'package:dashboard_manga_easy/core/config/app_helpes.dart';
 import 'package:dashboard_manga_easy/core/config/status_build_enum.dart';
 import 'package:flutter/material.dart';
 import 'package:manga_easy_sdk/manga_easy_sdk.dart';
@@ -13,5 +14,19 @@ abstract class IController extends ChangeNotifier {
     } catch (e) {
       Helps.log(e);
     }
+  }
+
+  void handlerError(e, BuildContext context) {
+    String message = 'Ocorreu um erro';
+    Helps.log(e);
+
+    if (e is ApiError) {
+      message = e.message;
+    }
+    AppHelps.confirmaDialog(
+      title: 'Error ⚠️😥',
+      content: message.replaceFirst('Exception:', ''),
+      context: context,
+    );
   }
 }
