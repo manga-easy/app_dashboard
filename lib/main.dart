73a5611e6f4951b5c3fad86139e9b19ba0f3405e
lucas <lucas.cardo123@hotmail.com>
2024-09-07 22:58:31 -0400

    remove dynamic theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
 import 'package:dashboard_manga_easy/core/core_module.dart';
 import 'package:dashboard_manga_easy/core/services/routers/service_route.dart';
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 
-bool isDemoUsingDynamicColors = false;
-
-// Fictitious brand color.
-const _brandBlue = Color(0xFF1E88E5);
-
-var lightCustomColors =
-    const CustomColors(danger: Color.fromARGB(255, 56, 53, 229));
-var darkCustomColors =
-    const CustomColors(danger: Color.fromARGB(255, 154, 198, 239));
 final di = GetIt.instance;
 Future<void> main() async {
   usePathUrlStrategy();
@@ -31,90 +21,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DynamicColorBuilder(
-      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-        ColorScheme lightColorScheme;
-        ColorScheme darkColorScheme;
-
-        if (lightDynamic != null && darkDynamic != null) {
-          // On Android S+ devices, use the provided dynamic color scheme.
-          // (Recommended) Harmonize the dynamic color scheme' built-in semantic colors.
-          lightColorScheme = lightDynamic.harmonized();
-          // (Optional) Customize the scheme as desired. For example, one might
-          // want to use a brand color to override the dynamic [ColorScheme.secondary].
-          lightColorScheme = lightColorScheme.copyWith(secondary: _brandBlue);
-          // (Optional) If applicable, harmonize custom colors.
-          lightCustomColors = lightCustomColors.harmonized(lightColorScheme);
-
-          // Repeat for the dark color scheme.
-          darkColorScheme = darkDynamic.harmonized();
-          darkColorScheme = darkColorScheme.copyWith(secondary: _brandBlue);
-          darkCustomColors = darkCustomColors.harmonized(darkColorScheme);
-
-          isDemoUsingDynamicColors = true; // ignore, only for demo purposes
-        } else {
-          // Otherwise, use fallback schemes.
-          lightColorScheme = ColorScheme.fromSeed(
-            seedColor: _brandBlue,
-          );
-          darkColorScheme = ColorScheme.fromSeed(
-            seedColor: _brandBlue,
-            brightness: Brightness.dark,
-          );
-        }
-
-        return MaterialApp.router(
-          theme: ThemeData(
-            colorScheme: lightColorScheme,
-            extensions: [lightCustomColors],
-          ),
-          darkTheme: ThemeData(
-            colorScheme: darkColorScheme,
-            extensions: [darkCustomColors],
-          ),
-          localizationsDelegates: const [
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('pt', 'BR'),
-            Locale('en'),
-          ],
-          routerConfig: ServiceRoute.of.routers,
-          debugShowCheckedModeBanner: false,
-        );
-      },
+    return MaterialApp.router(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('pt', 'BR'),
+        Locale('en'),
+      ],
+      theme: ThemeData.dark(),
+      routerConfig: ServiceRoute.of.routers,
+      debugShowCheckedModeBanner: false,
     );
-  }
-}
-
-@immutable
-class CustomColors extends ThemeExtension<CustomColors> {
-  const CustomColors({
-    required this.danger,
-  });
-
-  final Color? danger;
-
-  @override
-  CustomColors copyWith({Color? danger}) {
-    return CustomColors(
-      danger: danger ?? this.danger,
-    );
-  }
-
-  @override
-  CustomColors lerp(ThemeExtension<CustomColors>? other, double t) {
-    if (other is! CustomColors) {
-      return this;
-    }
-    return CustomColors(
-      danger: Color.lerp(danger, other.danger, t),
-    );
-  }
-
-  CustomColors harmonized(ColorScheme dynamic) {
-    return copyWith(danger: danger!.harmonizeWith(dynamic.primary));
   }
 }
